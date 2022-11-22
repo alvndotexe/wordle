79c5506ffc2d868091db8e883de42b84a1ff834e
alvin <al@pop-os.localdomain>
2022-11-22 22:32:19 +0100

    dotenv files are aids

--- hooks/Database.ts
+++ hooks/Database.ts
@@ -1,12 +1,9 @@
 import { PrismaClient } from "../generated/client/deno/edge.ts";
-import { config } from "https://deno.land/x/dotenv@v3.2.0/mod.ts";
-
-const env = config();
 
 export const prisma = new PrismaClient({
   datasources: {
     db: {
-      url: Deno.env.get("DATABASE_URL") ?? env.DATABASE_URL,
+      url: Deno.env.get("DATABASE_URL"),
     },
   },
 });
