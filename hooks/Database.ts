1c3df3263572f65b88d6700074df189802ff0144
alvin <al@pop-os.localdomain>
2022-12-01 15:04:35 +0100

    pain

--- hooks/Database.ts
+++ hooks/Database.ts
@@ -1,10 +1,15 @@
 import { PrismaClient } from "../generated/client/deno/edge.ts";
 import { config } from "https://deno.land/std@0.167.0/dotenv/mod.ts";
 
+import * as log from "https://deno.land/std@0.167.0/log/mod.ts";
+const pain = await config();
+log.info(Deno.env.get("DATABASE_URL"), Deno.env.get("DATA_PROXY_URL"), pain);
+
+const url = await config().then((c) => c["DATA_PROXY_URL"]);
 export const prisma = new PrismaClient({
   datasources: {
     db: {
-      url: Deno.env.get("DATABASE_URL"),
+      url: Deno.env.get("DATA_PROXY_URL") ?? url,
     },
   },
 });
